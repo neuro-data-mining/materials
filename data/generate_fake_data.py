979dd7dd7cc512a952fffcf1fcb74daab32024b0
klarnemann <klarnemann@berkeley.edu>
2015-08-24 14:48:12 -0700

    finalize generate_fake_data.py & pandas_demo.ipynb

--- data/generate_fake_data.py
+++ data/generate_fake_data.py
@@ -7,14 +7,16 @@ labeldf = pd.read_csv(labelf, names=['RegionName','Label'])
 roinames = labeldf.RegionName.values
 
 # Generate fake pet data
-subjects = ['Subject_%s' % (n) for n in range(1, 15)]
+subjects = ['S%i' % (s) for s in range(1, 15)]
 # PIB-PET
 sub_df = pd.DataFrame(np.random.permutation(subjects)[:10], columns=['Subject'])
 pib_pet_dat = np.random.gamma(shape=2., scale=0.5, size=800).reshape(10,80)
 pib_pet_df = pd.concat([sub_df, pd.DataFrame(pib_pet_dat, columns=roinames)], axis=1)
 pib_pet_df.to_excel('fake_pib_pet_data.xlsx')
-# FDG-PET
-sub_df = pd.DataFrame(np.random.permutation(subjects)[:8], columns=['Subject'])
-fdg_pet_dat = np.random.gamma(shape=2., scale=0.5, size=640).reshape(8,80)
-fdg_pet_df = pd.concat([sub_df, pd.DataFrame(fdg_pet_dat, columns=roinames)], axis=1)
-fdg_pet_df.to_excel('fake_fdg_pet_data.xlsx')
+# APOE Status
+sub_df = pd.DataFrame(np.random.permutation(subjects), columns=['Subject'])
+apoe_list = ['E2','E3','E4']
+apoe_dat = np.random.choice(apoe_list, 30).reshape(15,2)
+apoe_df = pd.concat([sub_df, pd.DataFrame(apoe_dat, \
+                                              columns=['APOE_Copy1', 'APOE_Copy2'])], axis=1)
+apoe_df.to_csv('fake_apoe_data.csv')
